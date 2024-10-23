bcfc84c94f2a75fb8b2b53dfe170b8b352abe6bc
Harry KR <harry@hskr.xyz>
2024-10-23 23:08:20 +0000

    Add compound furniture configuration to config.json

--- src/layout_generator.js
+++ src/layout_generator.js
@@ -13,6 +13,56 @@ const directions = {
   west: "west",
 };
 
+class Person {
+  constructor(config) {
+    this.name = config.name;
+    this.position = config.position;
+    this.facing_direction = config.facing_direction;
+    this.symbol = config.symbol;
+  }
+
+  moveAround(room) {
+    myRoom.grid[this.position.y][this.position.x] = " ";
+
+    const emptySpaces = room.findEmptySpacesAround(
+      this.position.x,
+      this.position.y
+    );
+    // console.log("empty spaces", emptySpaces);
+
+    const randomIndex = Math.floor(Math.random() * emptySpaces.length);
+    const newPosition = emptySpaces[randomIndex];
+    this.position.x = newPosition.x;
+    this.position.y = newPosition.y;
+
+    myRoom.grid[this.position.y][this.position.x] = this.symbol;
+  }
+}
+
+class Furniture {
+  constructor(config) {
+    this.name = config.name;
+    this.width = config.width;
+    this.length = config.length;
+    this.buffer = config.buffer;
+    this.colour = config.colour;
+    this.symbol = config.symbol;
+    this.facing_direction = config.facing_direction;
+    this.must_touch_wall = config.must_touch_wall;
+    this.position = config.position || { x: 0, y: 0 };
+    this.createdFurniture = config.createdFurniture;
+    this.which_side_touches_wall = config.which_side_touches_wall;
+  }
+}
+
+class CompoundFurniture {
+  constructor(config) {
+    this.name = config.name;
+    this.components = config.components;
+    this.facing_direction = config.facing_direction;
+  }
+}
+
 class Room {
   constructor(config) {
     this.width = config.room.width + 2 * config.room.wall_thickness;
@@ -35,6 +85,35 @@ class Room {
     return Array.from({ length: this.length }, () =>
       Array.from({ length: this.width }, () => " ")
     );
+  }
+
+  findEmptySpacesAround(x, y) {
+    const emptySpaces = [];
+    const directions = [
+      { dx: -1, dy: 0 }, // left
+      { dx: 1, dy: 0 }, // right
+      { dx: 0, dy: -1 }, // up
+      { dx: 0, dy: 1 }, // down
+      { dx: -1, dy: -1 }, // top-left
+      { dx: 1, dy: -1 }, // top-right
+      { dx: -1, dy: 1 }, // bottom-left
+      { dx: 1, dy: 1 }, // bottom-right
+    ];
+
+    for (const { dx, dy } of directions) {
+      const newX = x + dx;
+      const newY = y + dy;
+
+      if (this.isWithinBounds(newX, newY) && this.grid[newY][newX] === " ") {
+        emptySpaces.push({ x: newX, y: newY });
+      }
+    }
+
+    return emptySpaces;
+  }
+
+  isWithinBounds(x, y) {
+    return x >= 0 && x < this.width && y >= 0 && y < this.length;
   }
 
   placeWalls() {
@@ -168,92 +247,176 @@ class Room {
     }
   }
 
+  createSingleFurniture(furniture) {
+    // console.log(furniture);
+    console.log("creating:", furniture.name);
+    // Create a temporary grid for the furniture
+    const combinedLength =
+      furniture.length + furniture.buffer.north + furniture.buffer.south;
+    const combinedWidth =
+      furniture.width + furniture.buffer.east + furniture.buffer.west;
+    const grid = Array.from(
+      { length: combinedLength },
+      () => Array.from({ length: combinedWidth }, () => 0) // 0 represents empty space
+    );
+    // The first character will contain an arrow indicating the facing direction
+    let placedFirstCharacter = false;
+
+    let character = "";
+    const furnitureSymbol = furniture.symbol;
+    // i is the y
+    for (
+      let y = 0;
+      y < furniture.length + furniture.buffer.north + furniture.buffer.south;
+      y++
+    ) {
+      // j is the x
+      for (
+        let x = 0;
+        x < furniture.width + furniture.buffer.east + furniture.buffer.west;
+        x++
+      ) {
+        // Change character for north buffer
+        if (
+          y < furniture.buffer.north &&
+          x >= furniture.buffer.west &&
+          x < combinedWidth - furniture.buffer.east
+        ) {
+          character = ".";
+          // Change character for east buffer
+        } else if (
+          x > furniture.width + furniture.buffer.west - 1 &&
+          y >= furniture.buffer.north &&
+          y < combinedLength - furniture.buffer.south
+        ) {
+          character = ".";
+          // Change character for south buffer
+        } else if (
+          y > furniture.length + furniture.buffer.north - 1 &&
+          x > furniture.buffer.west - 1 &&
+          x < combinedWidth - furniture.buffer.east &&
+          y < combinedLength
+        ) {
+          character = ".";
+          // Change character for west buffer
+        } else if (
+          x < furniture.buffer.west &&
+          y >= furniture.buffer.north &&
+          y < combinedLength - furniture.buffer.south
+        ) {
+          character = ".";
+        } else if (
+          // Change character for furniture
+          x >= furniture.buffer.west &&
+          x <= furniture.buffer.east + furniture.width &&
+          y >= furniture.buffer.north &&
+          y < combinedLength - furniture.buffer.south
+        ) {
+          if (!placedFirstCharacter) {
+            console.log("facing direction:", furniture.facing_direction);
+            character = this.getFacingDirectionSymbol("north");
+            placedFirstCharacter = true;
+          } else {
+            character = furnitureSymbol;
+          }
+        } else {
+          character = " ";
+        }
+
+        grid[y][x] = character;
+      }
+    }
+    this.printFurniture(grid);
+    furniture.createdFurniture = grid;
+    return furniture;
+  }
+
+  placeComponentOnGrid(targetGrid, componentGrid, position) {
+    for (let y = 0; y < componentGrid.length; y++) {
+      for (let x = 0; x < componentGrid[0].length; x++) {
+        if (componentGrid[y][x] !== 0) {
+          targetGrid[position.y + y][position.x + x] = componentGrid[y][x];
+        }
+      }
+    }
+  }
+
   // instead of placing, create it and store it in an array and then try placing it.
 
   createFurniture(furnitures) {
     const createdFurnitures = furnitures.map((furniture) => {
-      // console.log(furniture);
-      console.log("creating:", furniture.name);
-      // Create a temporary grid for the furniture
-      const combinedLength =
-        furniture.length + furniture.buffer.north + furniture.buffer.south;
-      const combinedWidth =
-        furniture.width + furniture.buffer.east + furniture.buffer.west;
-      const grid = Array.from(
-        { length: combinedLength },
-        () => Array.from({ length: combinedWidth }, () => 0) // 0 represents empty space
-      );
-      // The first character will contain an arrow indicating the facing direction
-      let placedFirstCharacter = false;
+      // This is a compound furniture
+      // Has not been fully implemented yet
+      // Need to define specific placement for compound furniture inside the config
+      if (furniture.components) {
+        console.log("creating compound furniture");
+        // Handle compound furniture
+        const compoundGrid = this.createGrid();
 
-      let character = "";
-      const furnitureSymbol = furniture.symbol;
-      // i is the y
-      for (
-        let y = 0;
-        y < furniture.length + furniture.buffer.north + furniture.buffer.south;
-        y++
-      ) {
-        // j is the x
-        for (
-          let x = 0;
-          x < furniture.width + furniture.buffer.east + furniture.buffer.west;
-          x++
-        ) {
-          // Change character for north buffer
-          if (
-            y < furniture.buffer.north &&
-            x >= furniture.buffer.west &&
-            x < combinedWidth - furniture.buffer.east
-          ) {
-            character = ".";
-            // Change character for east buffer
-          } else if (
-            x > furniture.width + furniture.buffer.west - 1 &&
-            y >= furniture.buffer.north &&
-            y < combinedLength - furniture.buffer.south
-          ) {
-            character = ".";
-            // Change character for south buffer
-          } else if (
-            y > furniture.length + furniture.buffer.north - 1 &&
-            x > furniture.buffer.west - 1 &&
-            x < combinedWidth - furniture.buffer.east &&
-            y < combinedLength
-          ) {
-            character = ".";
-            // Change character for west buffer
-          } else if (
-            x < furniture.buffer.west &&
-            y >= furniture.buffer.north &&
-            y < combinedLength - furniture.buffer.south
-          ) {
-            character = ".";
-          } else if (
-            // Change character for furniture
-            x >= furniture.buffer.west &&
-            x <= furniture.buffer.east + furniture.width &&
-            y >= furniture.buffer.north &&
-            y < combinedLength - furniture.buffer.south
-          ) {
-            if (!placedFirstCharacter) {
-              console.log("facing direction:", furniture.facing_direction);
-              character = this.getFacingDirectionSymbol("north");
-              placedFirstCharacter = true;
-            } else {
-              character = furnitureSymbol;
-            }
-          } else {
-            character = " ";
+        // find the main component and process first
+        const mainComponent = new Furniture(
+          this.createSingleFurniture(
+            furniture.components.find((component) => component.main_component)
+          )
+        );
+
+        // the rest of the components need to be placed relative to the main component
+        // make an array of the components that are not the main component
+        const subComponents = furniture.components
+          .filter((component) => !component.main_component)
+          .map((component) => {
+            return new Furniture(this.createSingleFurniture(component));
+          });
+
+        // place the main component on the grid
+
+        this.placeComponentOnGrid(
+          compoundGrid,
+          mainComponent.createdFurniture,
+          {
+            x: 0,
+            y: 0,
           }
+        );
 
-          grid[y][x] = character;
-        }
+        console.log("subComponents", subComponents);
+
+        this.placeComponentOnGrid(
+          compoundGrid,
+          subComponents[0].createdFurniture,
+          {
+            x: subComponents[0].position.x,
+            y: subComponents[0].position.y,
+          }
+        );
+
+        this.printFurniture(compoundGrid);
+
+        // furniture.components.forEach((component) => {
+        //   const createdSubFurniture = new Furniture(
+        //     this.createSingleFurniture(component)
+        //   );
+        //   this.placeComponentOnGrid(
+        //     compoundGrid,
+        //     createdSubFurniture.createdFurniture,
+        //     component.position
+        //   );
+        // });
+        // return { ...furniture, grid: compoundGrid };
+
+        // remove all empty spaces from the grid
+        const filteredGrid = compoundGrid
+          .filter((row) => row.some((cell) => cell !== " ")) // Remove empty rows
+          .map((row) => row.filter((cell) => cell !== " ")); // Remove empty cells within rows
+        furniture.createdFurniture = filteredGrid;
+        return furniture;
+      } else {
+        const createdFurniture = this.createSingleFurniture(furniture);
+        console.log("created furniture", createdFurniture);
+        return new Furniture(this.createSingleFurniture(furniture));
       }
-      this.printFurniture(grid);
-      furniture.createdFurniture = grid;
-      return furniture;
     });
+
     return createdFurnitures;
   }
 
@@ -312,6 +475,10 @@ class Room {
 
   placeFurniture() {
     this.furniture.forEach((furniture) => {
+      if (typeof furniture === "undefined") {
+        return;
+      }
+      console.log("furniture", furniture);
       console.log("placing:", furniture.name);
       const roomWidth = this.width;
       const roomLength = this.length;
@@ -347,6 +514,7 @@ class Room {
                   ) {
                     // Only do this check on the first iteration
                     if (i === 0 && j === 0 && furniture.must_touch_wall) {
+                      console.log("furniture must touch wall");
                       // Check to see if the furniture needs to be placed on a wall
                       // The back of the furniture should touch a wall
                       // This is determined by comparing the furnitures orientation with the wall it is touching
@@ -361,10 +529,10 @@ class Room {
                       // Fourth compare both coridnates and see if the coordinates for the back of the furniture are
                       // in the list of coordinates for the space next to the wall
 
-                      console.log(
-                        "facing direction:",
-                        furniture.facing_direction
-                      ); // facing direction of the furniture
+                      // console.log(
+                      //   "facing direction:",
+                      //   furniture.facing_direction
+                      // ); // facing direction of the furniture
 
                       /**
                        * Returns the facing wall direction based on the given direction.
@@ -380,33 +548,47 @@ class Room {
                         return Object.values(directions)[newIndex];
                       };
 
-                      // Create a list of positions that the furniture can be placed by checking the BackFacingWall
-                      const backFacingWall = getBackFacingWall(
-                        furniture.facing_direction
-                      );
-                      console.log("backFacingWall:", backFacingWall);
+                      let touchingWall;
+
+                      // Currently not implemented
+                      // Needs changing
+                      if (furniture.which_side_touches_wall) {
+                        // touchingWall = furniture.which_side_touches_wall;
+                        // furniture.facing_direction = "north";
+                        touchingWall = getBackFacingWall(
+                          furniture.facing_direction
+                        );
+
+                        console.log("which side touches wall:", touchingWall);
+                      } else {
+                        // Create a list of positions that the furniture can be placed by checking the BackFacingWall
+                        touchingWall = getBackFacingWall(
+                          furniture.facing_direction
+                        );
+                      }
+
                       // get the open tiles to the left of the backFacingWall
                       const openTiles = [];
 
-                      if (backFacingWall === "north") {
+                      if (touchingWall === "north") {
                         for (let i = 0; i < roomWidth; i++) {
                           if (this.grid[1][i] === " ") {
                             openTiles.push([1, i]);
                           }
                         }
-                      } else if (backFacingWall === "south") {
+                      } else if (touchingWall === "south") {
                         for (let i = 0; i < roomWidth; i++) {
                           if (this.grid[roomLength - 2][i] === " ") {
                             openTiles.push([roomLength - 2, i]);
                           }
                         }
-                      } else if (backFacingWall === "west") {
+                      } else if (touchingWall === "west") {
                         for (let i = 0; i < roomLength; i++) {
                           if (this.grid[i][roomWidth - 2] === " ") {
                             openTiles.push(i);
                           }
                         }
-                      } else if (backFacingWall === "east") {
+                      } else if (touchingWall === "east") {
                         // gets the tiles to the left of the backFacingWall
                         for (let i = 0; i < roomLength; i++) {
                           // console.log(this.grid[i][roomWidth - 2]);
@@ -415,13 +597,18 @@ class Room {
                           }
                         }
                       }
-                      console.log("openTiles:", openTiles);
-                      console.log("this grid");
-                      console.log(this.grid[0][1]);
+                      // console.log("openTiles:", openTiles);
+                      // console.log("this grid");
+                      // console.log(this.grid[0][1]);
 
                       // gets the back row of the furniture based on orientation
                       const backRowPositionsOfFurniture = [];
-                      console.log(createdFurniture);
+                      // console.log(createdFurniture);
+
+                      // Need to implement for west and east
+                      // If i determine that the furniture must touch a specific wall,
+                      // The "back" of the furniture will be the side that is touching the wall
+                      // so if i say the furniture is facing north but touching the east wall, it should have a back that is define as the east
 
                       if (furniture.facing_direction === "north") {
                         createdFurniture[createdFurniture.length - 1].forEach(
@@ -445,11 +632,11 @@ class Room {
                         // Back row is the last column in the array
                         // NEED TO IMPLEMENT
                       }
-                      console.log("current position:", y, x);
-                      console.log(
-                        "backRowPositionsOfFurniture:",
-                        backRowPositionsOfFurniture
-                      );
+                      // console.log("current position:", y, x);
+                      // console.log(
+                      //   "backRowPositionsOfFurniture:",
+                      //   backRowPositionsOfFurniture
+                      // );
 
                       // Check if all back row positions are in open tiles
                       const allPositionsValid =
@@ -597,4 +784,25 @@ class Room {
 const config = readConfig("./src/config.json");
 const myRoom = new Room(config);
 myRoom.printRoom();
-console.log(myRoom.createdFurniture);
+
+const myPerson = new Person({
+  name: "John",
+  position: { x: 10, y: 1 },
+  facing_direction: "north",
+  symbol: "P",
+});
+
+myRoom.grid[myPerson.position.y][myPerson.position.x] = myPerson.symbol;
+
+for (let i = 0; i < 100; i++) {
+  setTimeout(() => {
+    console.clear();
+    myPerson.moveAround(myRoom);
+    // console.log(myPerson.position);
+    myRoom.printRoom();
+  }, i * 100);
+}
+
+// myPerson.moveAround(myRoom);
+
+// myRoom.printRoom();
